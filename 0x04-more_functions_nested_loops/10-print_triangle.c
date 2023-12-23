c14cd2fb956d4c5c99d5e60d2c31dc5ef16bafdf
Geomohamed <geoa977@gmail.com>
2023-12-22 18:54:53 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,18 +15,18 @@ void print_triangle(int size)
 	if (size <= 0)
 	       _putchar('\n')
 	else
-        {
-              for (hght = 1; hght <= size; hght++)
-             {
-                     for (base = 1; base <= siz; base++)
-		     {
-			     if (hght + base) <= size)
-				     _putchar(' ');
-		             else
-				     _putchar('#');
-		     }
-		     _putchar('\n');
-	     }
-        }
+	{
+		for (hght = 1; hght <= size; hght++)
+		{
+		       	for (base = 1; base <= siz; base++)
+			{
+				if (hght + base) <= size)
+					_putchar(' ');
+				else
+					_putchar('#'):
+			}
+			_putchar('\n');
+		}
+	}
 }
 	          		     
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,13 +9,13 @@
 
 void print_line(int n)
 {
-	int 1nchar;
+	int 1nchr;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (1nchar = 1; 1nchar <= n; 1nchar++)
+		for (1nchar = 1; 1nchr <= n; 1nchr++)
 			-putchar('_');
 		_putchar('\n');
 	}
