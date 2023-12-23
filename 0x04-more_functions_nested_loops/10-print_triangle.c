ec79f4b4aed9a2e86528c4c8a04fe6ac5ed8bd0d
Geomohamed <geoa977@gmail.com>
2023-12-22 19:24:48 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,20 +13,19 @@ void print_triangle(int size)
 	int hght, base;
 
 	if (size <= 0)
-	       _putchar('\n')
+		_putchar('\n');
 	else
 	{
 		for (hght = 1; hght <= size; hght++)
 		{
-		       	for (base = 1; base <= siz; base++)
+			for (base = 1; base <= size; base++)
 			{
-				if (hght + base) <= size)
+				if ((hght + base) <= size)
 					_putchar(' ');
 				else
-					_putchar('#'):
+					_putchar('#');
 			}
 			_putchar('\n');
 		}
 	}
 }
-	          		     
